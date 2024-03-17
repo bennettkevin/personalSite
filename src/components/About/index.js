80509132c5692f179b85bcd059716e3bda1ff0f3
Kevin Bennett <kevinbennett@Kevins-MacBook-Air.local>
2024-03-16 23:27:20 -0400

    Added photo of Greensboro project, and made some text additions to about page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,17 @@
+import GreensboroBridge from '../../Assets/Images/GreensboroBridge.JPG'
+
 const About = () => (
     <div className="container about-page">
         <div className="text-field">
-            <h1>About Kevin Bennet</h1>
+            <h1>About ME</h1>
+            <section className="main-section">
+                <header className="section-header">History</header>
+                <p>Upon highschool graduation I started college at GTCC, this would not be my last time in school, in fact 
+                    at the halfway point of my second semester I would drop out. This lead to my career in construction, a total 
+                    of 6 years on heavy highway/bridge construction. Below is the first bridge project I worked on it is a section 
+                    of the urban loop in Greensboro, NC.</p>
+                    <img src={GreensboroBridge} alt="Greensboro Urban loop Battleground Ave section."/>
+            </section>
         </div>
     </div>
 )
