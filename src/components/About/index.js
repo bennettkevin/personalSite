e7fc2314ec2e91e9256cbab40770b7722416117d
Kevin Bennett <bennettkevin45@outlook.com>
2024-07-24 00:12:31 -0400

    Added some text to about, hid the scrollbar.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,7 +11,12 @@ const About = () => (
                     at the halfway point of my second semester I would drop out. This lead to my career in construction, a total 
                     of 6 years on heavy highway/bridge construction. Below is the first bridge project I worked on it is a section 
                     of the urban loop in Greensboro, NC.</p>
-                <img src={GreensboroBridge} alt="Greensboro Urban loop Battleground Ave section."/>
+                <img id="greensboro" src={GreensboroBridge} alt="Greensboro Urban loop Battleground Ave section."/>
+                <p id="bottomPara">Currently enrolled at GTCC again pursuing an AS Computer Science. With plans to attend university and obtain my BS in Computer Science.
+                    Technology has been a passion of mine for my entire life, and I am enjoying the process of learning more about it in ways that allow 
+                    me to not only progress in a way that can benifit me from a career standpoint but also to pursue the development of personal projects.
+                    Feel free to check out some of the things I have worked on and projects I have built while learning on <a target='_blank' rel='noreferrer' href='https://github.com/bennettkevin'>GitHub.</a>
+                    </p>
             </section>
         </div>
     </div>
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,5 +1,15 @@
+import './index.scss'
+
 const Work = () => (
-    "Hello from Work"
+    
+    <div className="main-text">
+        <header>Projects</header>
+        <p>
+            I'm currently building on the list of projects that I've contributed to. As of now the list can be seen
+            below in no particular order. 
+        </p>
+    </div>
+    
 )
 
 export default Work
